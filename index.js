38bdc556bd97dd242972a62d455750d6bbaac733
Andrew Klimenyuk <andrew.klmn@gmail.com>
2021-04-23 14:30:05 +0300

    Add: render functionality

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-'use strict';
+import { rawNewsData } from './fixtures';
 
 const newsAPIkey = 'c66d33daf75d4760b60252aef3f48983';
+const NUMBER_OF_SHOWED_NEWS_ITEMS = 10;
 
-let i;
+if (module.hot) {
+  module.hot.accept();
+}
 
-// eslint-disable-next-line no-console
-console.log('Hello there...');
+window.dataStore = {
+  news: rawNewsData,
+  newsItemShow: NUMBER_OF_SHOWED_NEWS_ITEMS,
+};
+
+window.renderApp = renderApp;
+renderApp();
+
+function renderApp() {
+  document.querySelector('.root').innerHTML = `
+        ${app()}
+    `;
+}
+
+function app() {
+  return `<div class="container">
+  ${newsList(dataStore)}
+</div>`;
+}
+
+function newsList({ news, newsItemShow }) {
+  return news.articles.splice(0, newsItemShow).reduce((acc, currentArticle) => {
+    return (acc += `
+      <div class="news-item">
+        <h3>${currentArticle.title}</h3>
+        <img class="news-img" src="${currentArticle.urlToImage}"/>
+        <p>${currentArticle.content}</p>
+        Source is here <a target="_blank" href="${currentArticle.url}">${currentArticle.url}</a>
+      </div>
+    `);
+  }, '');
+}
