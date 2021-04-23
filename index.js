3f08a3b941e67d34fbcd7bf3228519318b3e1d69
Andrew Klimenyuk <andrew.klmn@gmail.com>
2021-04-23 13:29:28 +0300

    Just check linter

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const APIkey = 'c66d33daf75d4760b60252aef3f48983';
+'use strict';
 
+const newsAPIkey = 'c66d33daf75d4760b60252aef3f48983';
+
+let i;
+
+// eslint-disable-next-line no-console
 console.log('Hello there...');
