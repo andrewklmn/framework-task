7a57ef8b9602918e00aee7f3881251c4e9409839
Andrew Klmn <andrew.klmn@gmail.com>
2021-06-18 01:22:56 +0300

    Replace: inline CSS with classNames in Preloader

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
-import { contentClass } from './../style.css';
+import classNames from 'classnames';
+import { contentClass, preloader } from './../style.css';
 import preloaderImage from './../img/preloader.gif';
 
 export default function Preloader() {
   return (
-    <div className={contentClass} style={{ paddingTop: '50px' }}> // TODO
+    <div className={classNames(contentClass, preloader)}>
       <img src={preloaderImage} alt="News image" />
     </div>
   );
