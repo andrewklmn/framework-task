978babaa8eaa9c92891bab99ad37fe89b02ab37e
Andrew Klmn <andrew.klmn@gmail.com>
2021-06-18 01:07:18 +0300

    Replace: inline CSS with classNames in ErrorWindow

--- src/components/ErrorWindow.js
+++ src/components/ErrorWindow.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import classNames from 'classname'
-import { contentClass } from '../style.css';
+import classNames from 'classname';
+import { contentClass, error } from '../style.css';
 
 export default function ErrorWindow({ error }) {
-  return (
-    <div className={contentClass} style={{ color: 'red' }}> // TODO
-      {error}
-    </div>
-  );
+  return <div className={classNames(contentClass, error)}>{error}</div>;
 }
