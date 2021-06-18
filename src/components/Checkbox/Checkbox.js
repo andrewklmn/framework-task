516afaf12345e18179f0bd2bcfd2c7046a63c995
Andrew Klmn <andrew.klmn@gmail.com>
2021-06-18 11:34:17 +0300

    Add: classNames to Checkbox component

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import classNames from 'classnames';
 import styles from './Checkbox.css';
 
 export default function Checkbox({ label = '', onChange = null }) {
   return (
-    <label className={[styles.check, styles.option]}>
+    <label className={classNames(styles.check, styles.option)}>
       <input className={styles.check__input} type="checkbox" onChange={onChange} />
       <span className={styles.check__box}></span>
       {label}
